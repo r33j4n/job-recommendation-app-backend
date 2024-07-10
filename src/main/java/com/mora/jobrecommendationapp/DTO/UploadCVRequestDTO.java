641e94635951eff392d4ada9ed3dda8b9a70bff8
varakiVakeesan <varakivakeesan23@gmail.com>
2024-07-11 03:44:11 +0530

    isCVUploaded variable added.

---
+++ src/main/java/com/mora/jobrecommendationapp/DTO/UploadCVRequestDTO.java
@@ -0,0 +1,12 @@
+package com.mora.jobrecommendationapp.DTO;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UploadCVRequestDTO {
+
+    private Boolean is_cv_uploaded;
+
+}
---
+++ src/main/java/com/mora/jobrecommendationapp/DTO/UploadCVResponseDTO.java
@@ -0,0 +1,10 @@
+package com.mora.jobrecommendationapp.DTO;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UploadCVResponseDTO {
+    private String message;
+}
