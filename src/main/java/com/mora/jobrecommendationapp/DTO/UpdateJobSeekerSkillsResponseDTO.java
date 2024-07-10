9fd53def000060956f63bd3d0f18183340292936
varakiVakeesan <varakivakeesan23@gmail.com>
2024-07-11 04:03:46 +0530

    Update Skills implemented

---
+++ src/main/java/com/mora/jobrecommendationapp/DTO/UpdateJobSeekerSkillsResponseDTO.java
@@ -0,0 +1,11 @@
+package com.mora.jobrecommendationapp.DTO;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateJobSeekerSkillsResponseDTO {
+
+    private String message;
+}
